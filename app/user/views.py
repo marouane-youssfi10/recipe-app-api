7350ec6504837ddb3ff0aec54e07e29a63abbeb9
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-03-30 13:18:26 +0000

    create serializers.py and setting our urls

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user is the system"""
+    serializer_class = UserSerializer
+
+
