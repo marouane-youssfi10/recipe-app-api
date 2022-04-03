8f46c90e7138280b0410b1fcd7af4e853cc46a3e
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-04-03 22:46:37 +0000

    testing

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from ..models import Tag
+from ..models import Tag, Ingredient
 
 
 def sample_user(email='test@gmail.com', password='testpass'):
@@ -48,3 +48,11 @@ class ModelTests(TestCase):
             name='Vegan'
         )
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = Ingredient.objects.create(
+            user=sample_user(),
+            name='Customer'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
