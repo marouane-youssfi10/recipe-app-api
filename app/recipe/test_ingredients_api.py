4c817239e16b1d5d3a3090a7f7417c09e1c5c398
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-04-03 23:39:21 +0000

    testing

--- app/recipe/test_ingredients_api.py
+++ app/recipe/test_ingredients_api.py
@@ -8,3 +8,19 @@ from rest_framework.test import APIClient
 from app.core.models import Ingredient
 
 from app.recipe.serializers import IngredientSerializer
+
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+class PublicIngredientsApiTests(TestCase):
+    """Test the publicly available ingredients API"""
+    
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required to access the endpoint"""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
