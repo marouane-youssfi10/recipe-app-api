5d1c74f836250c0bedb5063c6c46c28c26006387
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-04-04 15:09:35 +0000

    testing

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -79,19 +79,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def upload_image(self, request, pk=None):
         """Upload an image to a recipe"""
         recipe = self.get_object()
-        serializer = self.get_serializer(
-            recipe,
-            data=request.data
-        )
+        serializer = self.get_serializer(recipe, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK
-            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
